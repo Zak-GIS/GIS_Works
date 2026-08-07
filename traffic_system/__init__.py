8d3328236d39634f7346e9a060b06bc465ab75e6
Zak-GIS <aleksandrovt2116@yandex.ru>
2026-08-07 16:37:51 +0300

    .

--- traffic_system/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import BaseTrafficSensor
-from .manager import TrafficManager
-from .sensors import SpeedCamera, TrafficDensitySensor
-
-__all__ = ["BaseTrafficSensor", "SpeedCamera", "TrafficDensitySensor", "TrafficManager"]
--- traffic_system/base.py
+++
@@ -1,8 +0,0 @@
-class BaseTrafficSensor:
-    def __init__(self, sensor_id, location, is_active):
-        self.sensor_id = sensor_id
-        self.location = location
-        self.is_active = is_active
-
-    def get_info(self):
-        return f"Sensor {self.sensor_id} at {self.location} - {self.is_active}"
